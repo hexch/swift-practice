a02ebc8a4b1b5a419cbc6e6966e612872aae94f6
何　暁川 <>
2019-10-01 20:19:26 +0900

    init

---
+++ Swift-Practice1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift-Practice
+//
+//  Created by 何　暁川 on 2019/10/01.
+//  Copyright © 2019 Hexch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
